1a6d3929961c48c5672b4c42a7b6a933a3408d4e
vinay-in <vk243738@gmail.com>
2021-12-22 13:36:37 +0530

    this is my commit

---
+++ src/main/java/vngroups/in/demoproject1/App.java
@@ -0,0 +1,13 @@
+package vngroups.in.demoproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/vngroups/in/demoproject1/AppTest.java
@@ -0,0 +1,20 @@
+package vngroups.in.demoproject1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
